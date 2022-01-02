c01404e2d9f1c3c9ead46f6b5ed8eefd47c4f0fb
N0I0C0K <nick131410@aliyun.com>
2022-01-02 10:51:02 +0800

    第一次更新, 已经基本实现了逻辑

---
+++ Translater.cs
@@ -0,0 +1,144 @@
+using Wox.Plugin;
+using Wox.Plugin.Logger;
+using Translater.utils;
+
+namespace Translater
+{
+    public class Translater : IPlugin
+    {
+        // Localized name
+        public string Name { get; } = "Translater";
+
+        // Localized description
+        public string Description { get; } = "Test";
+
+        /// <summary>
+        /// save the last query.
+        /// </summary>
+        public string queryContent = "";
+        public PluginMetadata queryMetaData = null;
+        public IPublicAPI publicAPI = null;
+        /// <summary>
+        /// save the truely query time.
+        /// </summary>
+        public int queryTimes = 0;
+        public Action<string, bool> changeQuery;
+        public SynchronizationContext mainThreadContext;
+        public List<Result> Query(Query query)
+        {
+            List<Result> results = new List<Result>();
+            Result infoResult = new Result()
+            {
+                Title = queryMetaData.QueryCount.ToString(),
+                SubTitle = string.Format("{0} {1}", queryTimes.ToString(), this.queryContent)
+            };
+            results.Add(infoResult);
+
+            //if two are equal, it's the true query.
+            if (query.RawQuery == queryContent)
+            {
+                queryTimes++;
+                infoResult.SubTitle = string.Format("{0} {1}", queryTimes.ToString(), this.queryContent);
+                Log.Info(string.Format("catch a query {0}", queryContent), typeof(Translater));
+                try
+                {
+                    TranslateResponse translateResponse = Utils.TranslateZhToEn(query.Search);
+                    if (translateResponse.error_code != 52000)
+                    {
+                        throw new Exception(string.Format("Error in translate, error code:{0}", translateResponse.error_code));
+                    }
+                    results.Add(new Result()
+                    {
+                        Title = translateResponse.trans_result[0].dst,
+                        SubTitle = translateResponse.trans_result[0].src,
+                        Action = e =>
+                        {
+                            return false;
+                        }
+                    });
+                }
+                catch (System.Exception ex)
+                {
+                    results.Add(new Result()
+                    {
+                        Title = ex.Message,
+                        SubTitle = ex.ToString(),
+                        Action = e =>
+                        {
+                            return false;
+                        }
+                    });
+                    Task task = Task.Factory.StartNew(() =>
+                    {
+                        Thread.Sleep(500);
+                        changeQuery(this.queryContent, true);
+                    });
+                }
+            }
+            //else the user is continue typing.
+            else
+            {
+                queryContent = query.RawQuery;
+                infoResult.SubTitle = string.Format("{0} {1}", queryTimes.ToString(), this.queryContent);
+                Task task = Task.Factory.StartNew(
+                (queryRaw) =>
+                {
+                    if (queryRaw != null)
+                    {
+                        Thread.Sleep(500);
+                        Log.Info(string.Format("{0} == {1}", queryRaw.ToString(), this.queryContent), typeof(Translater));
+                        if (queryRaw.ToString() == this.queryContent)
+                        {
+                            changeQuery(this.queryContent, true);
+                        }
+                    }
+                }, state: queryContent);
+                results.Add(new Result()
+                {
+                    Title = task.Id.ToString(),
+                    SubTitle = task.Status.ToString(),
+                    Action = e =>
+                    {
+                        Clipboard.SetDataObject(task.Status.ToString());
+                        changeQuery(this.queryContent, true);
+                        return true;
+                    }
+                });
+            }
+            return results;
+        }
+        public void Init(PluginInitContext context)
+        {
+            Log.Info("translater init", typeof(Translater));
+            queryMetaData = context.CurrentPluginMetadata;
+            publicAPI = context.API;
+            this.mainThreadContext = SynchronizationContext.Current;
+            this.changeQuery = (_query, _force) =>
+            {
+                Log.Info($"{_query} == {this.queryContent} true, start change query", typeof(Translater));
+                try
+                {
+                    this.mainThreadContext!.Send((s) =>
+                    {
+                        try
+                        {
+                            publicAPI.ChangeQuery(this.queryContent, true);
+                        }
+                        catch (Exception ex)
+                        {
+                            Log.Exception("Error to change query", ex, typeof(Translater));
+                        }
+                        finally
+                        {
+                            Log.Info("end change query", typeof(Translater));
+                        }
+                    }, null);
+                }
+                catch (Exception ex)
+                {
+                    Log.Exception("Error in send mainContext", ex, typeof(Translater));
+                }
+            };
+        }
+    }
+}
---
+++ Translater.utils.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Web;
+using System.Security.Cryptography;
+using System.Runtime.Serialization.Json;
+
+namespace Translater.utils
+{
+    public class ResItems
+    {
+        public string src;
+        public string dst;
+    }
+    public class TranslateResponse
+    {
+
+        public string from;
+        public string to;
+        public int error_code = 52000;
+        public List<ResItems> trans_result;
+    }
+
+    public class Utils
+    {
+        public static string appid = "20211228001040502";
+        public static string secretKey = "Htvq0G7dc2M2MIjHzTYH";
+        public static string GETHttp(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "text/html;charset=UTF-8";
+            request.UserAgent = null;
+            request.Timeout = 6000;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+            string retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+            return retString;
+        }
+
+        public static TranslateResponse TranslateZhToEn(string query)
+        {
+            string from = "zh", to = "en";
+            string salt = new Random().Next(100000).ToString();
+            string sign = EncryptString(appid + query + salt + secretKey);
+            string url = string.Format("http://api.fanyi.baidu.com/api/trans/vip/translate?q={0}&from={1}&to={2}&appid={3}&salt={4}&sign={5}",
+                                        query, from, to, appid, salt, sign);
+            string res = GETHttp(url);
+            TranslateResponse data = null;
+            using (MemoryStream memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(res)))
+            {
+                DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(TranslateResponse));
+                data = (TranslateResponse)jsonSerializer.ReadObject(memoryStream);
+            }
+            return data;
+        }
+        public static string TranslateZhToEnRaw(string query)
+        {
+            string from = "zh", to = "en";
+            string salt = new Random().Next(100000).ToString();
+            string sign = EncryptString(appid + query + salt + secretKey);
+            string url = string.Format("http://api.fanyi.baidu.com/api/trans/vip/translate?q={0}&from={1}&to={2}&appid={3}&salt={4}&sign={5}",
+                                        query, from, to, appid, salt, sign);
+            string res = GETHttp(url);
+            return res;
+        }
+        public static string EncryptString(string str)
+        {
+            MD5 md5 = MD5.Create();
+            // 将字符串转换成字节数组
+            byte[] byteOld = Encoding.UTF8.GetBytes(str);
+            // 调用加密方法
+            byte[] byteNew = md5.ComputeHash(byteOld);
+            // 将加密结果转换为字符串
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in byteNew)
+            {
+                // 将字节转换成16进制表示的字符串，
+                sb.Append(b.ToString("x2"));
+            }
+            // 返回加密的字符串
+            return sb.ToString();
+        }
+    }
+}
