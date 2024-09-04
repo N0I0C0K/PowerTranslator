bf4c9e2cb0c39f3132a0f04a0c1a8a63766a3d1a
Ntkskwk <natsukawa247@outlook.com>
2024-09-05 06:28:13 +0800

    fix: 修复工作流

--- lib.bat
+++ lib.bat
@@ -1,4 +1,4 @@
-mkdir -p lib
+mkdir -p -e lib
 copy "C:\Program Files\PowerToys\PowerToys.Common.UI.dll" "lib\PowerToys.Common.UI.dll" /y
 copy "C:\Program Files\PowerToys\PowerToys.ManagedCommon.dll" "lib\PowerToys.ManagedCommon.dll" /y
 copy "C:\Program Files\PowerToys\PowerToys.Settings.UI.Lib.dll" "lib\PowerToys.Settings.UI.Lib.dll" /y
