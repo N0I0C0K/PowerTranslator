10ddb869ce9ac5973ce3375d30d4d0333d263091
Ntkskwk <natsukawa247@outlook.com>
2024-09-04 16:57:20 +0800

    feat: 安装时暂时关闭powertoys

--- install.bat
+++ install.bat
@@ -1,9 +1,18 @@
 @echo off
+
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+
 set "source=%cd%\Translator"
 set "destination=%LOCALAPPDATA%\Microsoft\PowerToys\PowerToys Run\Plugins\"
 
 echo Moving directory...
+
+taskkill /f /im PowerToys.exe
+
 move "%source%" "%destination%"
 
 echo Move completed.
+
+run "C:\Program Files\PowerToys\PowerToys.exe"
+
 pause
